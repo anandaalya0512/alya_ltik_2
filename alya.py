c1443bc54ff5ac6a378baffba388c39332976adc
anandaalya0512 <anandaalya0512@upi.edu>
2024-11-10 13:08:02 +0700

    mulai

---
+++ alya.py
@@ -0,0 +1,8 @@
+tugas = input("Apakah Ibu puas atau tidak? : ")
+
+if tugas == "puas":
+    print("Terima kasih, saya sangat senang.")
+elif tugas == "tidak":
+    print("Maaf, saya akan lebih berusaha lagi.")
+else:
+    print("Error. Pilihannya hanya ya atau tidak.")
